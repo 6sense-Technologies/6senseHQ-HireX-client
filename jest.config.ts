b66fdbd89d65e1df7132fc1d032a803f403b2157
Khan Atik Faisal <khanatik1176@gmail.com>
2025-01-10 11:23:07 +0600

    fixing-codecy

--- jest.config.ts
+++ jest.config.ts
@@ -6,27 +6,34 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig: Config = {
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(next-auth|@auth/core)/)',
-  ],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  testEnvironment: 'jsdom',
+  // extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  // transform: {
+  //   '^.+\\.(ts|tsx)$': 'babel-jest',
+  //   '^.+\\.(js|jsx)$': 'babel-jest',
+  // },
+  // transformIgnorePatterns: [
+  //   'node_modules/(?!(next-auth|@auth/core)/)',
+  // ],
+  // moduleNameMapper: {
+  //   '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  //   '^@/(.*)$': '<rootDir>/$1',
+  // },
+  // testEnvironment: 'jsdom',
+  // coverageProvider: 'v8',
+  // cacheDirectory: '.jest-cache',
+  // coverageDirectory: 'coverage',
+  // coverageReporters: ['html', 'lcov', 'text-summary'],
+  // collectCoverage: true,
+  // testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  // setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  
   testMatch: ['<rootDir>/__test__/**/*.test.(js|jsx|ts|tsx)'],
-  coverageProvider: 'v8',
-  cacheDirectory: '.jest-cache',
-  coverageDirectory: '.jest-coverage',
-  coverageReporters: ['html', 'lcov', 'text-summary'],
-  collectCoverage: true,
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
 };
 
 // Ensure next/jest can load the Next.js config
